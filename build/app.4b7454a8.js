63b68e15b2293d0e87da4e285006b6933140943a
Alexandra Caulea <alexandra.caulea@gmail.com>
2020-05-02 19:53:11 +0300

    Update link

--- build/app.4b7454a8.js
+++ build/app.4b7454a8.js
@@ -2,30 +2,30 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 module.exports="api-fetch-jobs.d4064126.jpg";
 },{}],"YPXr":[function(require,module,exports) {
 module.exports="fcc-celsius-to-fahrenheit-convertor.c8868e4f.jpg";
-},{}],"eFgf":[function(require,module,exports) {
-module.exports="fcc-survey-form.f8768186.jpg";
-},{}],"n1qK":[function(require,module,exports) {
-module.exports="fcc-technical-documentation-page.16cf0502.jpg";
 },{}],"MmsR":[function(require,module,exports) {
 module.exports="fcc-product-landing-page.29536750.jpg";
+},{}],"eFgf":[function(require,module,exports) {
+module.exports="fcc-survey-form.f8768186.jpg";
 },{}],"a340":[function(require,module,exports) {
 module.exports="fcc-tribute-page.4fa4c75c.jpg";
-},{}],"vE9J":[function(require,module,exports) {
-module.exports="io-2.af92f2c0.jpg";
+},{}],"n1qK":[function(require,module,exports) {
+module.exports="fcc-technical-documentation-page.16cf0502.jpg";
 },{}],"iu3v":[function(require,module,exports) {
 module.exports="io-1.7393f5b4.jpg";
+},{}],"vE9J":[function(require,module,exports) {
+module.exports="io-2.af92f2c0.jpg";
 },{}],"afdl":[function(require,module,exports) {
 module.exports="layout-1.c62dd2ab.jpg";
+},{}],"AW2X":[function(require,module,exports) {
+module.exports="layout-2.85d77073.jpg";
 },{}],"GTvt":[function(require,module,exports) {
 module.exports="layout-3.d63804af.jpg";
 },{}],"eRGn":[function(require,module,exports) {
 module.exports="layout-4.75f5e2dc.jpg";
-},{}],"AW2X":[function(require,module,exports) {
-module.exports="layout-2.85d77073.jpg";
-},{}],"yfGi":[function(require,module,exports) {
-module.exports="layout-5.507ae2c5.jpg";
 },{}],"suTb":[function(require,module,exports) {
 module.exports="layout-6.4f1e7b98.jpg";
+},{}],"yfGi":[function(require,module,exports) {
+module.exports="layout-5.507ae2c5.jpg";
 },{}],"eO3I":[function(require,module,exports) {
 module.exports="layout-7.26b2f0d2.jpg";
 },{}],"YMHG":[function(require,module,exports) {
@@ -38,15 +38,15 @@ module.exports="react-emoji-generator.3f5707b2.jpg";
 module.exports="ui-features-section.fbafd860.jpg";
 },{}],"LZNF":[function(require,module,exports) {
 module.exports="ui-pricing-tables.33ad2fdd.jpg";
-},{}],"FCE7":[function(require,module,exports) {
-module.exports="ui-tabs.6bc379ea.jpg";
 },{}],"vNbd":[function(require,module,exports) {
 module.exports="ui-product-card.12895cd3.jpg";
+},{}],"FCE7":[function(require,module,exports) {
+module.exports="ui-tabs.6bc379ea.jpg";
 },{}],"e4Wr":[function(require,module,exports) {
 module.exports="ui-user-profile-card.49f2195a.jpg";
 },{}],"m7Bz":[function(require,module,exports) {
-module.exports={"api-fetch-jobs":require("./api-fetch-jobs.jpg"),"fcc-celsius-to-fahrenheit-convertor":require("./fcc-celsius-to-fahrenheit-convertor.jpg"),"fcc-survey-form":require("./fcc-survey-form.jpg"),"fcc-technical-documentation-page":require("./fcc-technical-documentation-page.jpg"),"fcc-product-landing-page":require("./fcc-product-landing-page.jpg"),"fcc-tribute-page":require("./fcc-tribute-page.jpg"),"io-2":require("./io-2.jpg"),"io-1":require("./io-1.jpg"),"layout-1":require("./layout-1.jpg"),"layout-3":require("./layout-3.jpg"),"layout-4":require("./layout-4.jpg"),"layout-2":require("./layout-2.jpg"),"layout-5":require("./layout-5.jpg"),"layout-6":require("./layout-6.jpg"),"layout-7":require("./layout-7.jpg"),"layout-8":require("./layout-8.jpg"),"layout-9":require("./layout-9.jpg"),"react-emoji-generator":require("./react-emoji-generator.jpg"),"ui-features-section":require("./ui-features-section.jpg"),"ui-pricing-tables":require("./ui-pricing-tables.jpg"),"ui-tabs":require("./ui-tabs.jpg"),"ui-product-card":require("./ui-product-card.jpg"),"ui-user-profile-card":require("./ui-user-profile-card.jpg")};
-},{"./api-fetch-jobs.jpg":"tnYU","./fcc-celsius-to-fahrenheit-convertor.jpg":"YPXr","./fcc-survey-form.jpg":"eFgf","./fcc-technical-documentation-page.jpg":"n1qK","./fcc-product-landing-page.jpg":"MmsR","./fcc-tribute-page.jpg":"a340","./io-2.jpg":"vE9J","./io-1.jpg":"iu3v","./layout-1.jpg":"afdl","./layout-3.jpg":"GTvt","./layout-4.jpg":"eRGn","./layout-2.jpg":"AW2X","./layout-5.jpg":"yfGi","./layout-6.jpg":"suTb","./layout-7.jpg":"eO3I","./layout-8.jpg":"YMHG","./layout-9.jpg":"FkQ3","./react-emoji-generator.jpg":"obdG","./ui-features-section.jpg":"I2ti","./ui-pricing-tables.jpg":"LZNF","./ui-tabs.jpg":"FCE7","./ui-product-card.jpg":"vNbd","./ui-user-profile-card.jpg":"e4Wr"}],"QdeU":[function(require,module,exports) {
+module.exports={"api-fetch-jobs":require("./api-fetch-jobs.jpg"),"fcc-celsius-to-fahrenheit-convertor":require("./fcc-celsius-to-fahrenheit-convertor.jpg"),"fcc-product-landing-page":require("./fcc-product-landing-page.jpg"),"fcc-survey-form":require("./fcc-survey-form.jpg"),"fcc-tribute-page":require("./fcc-tribute-page.jpg"),"fcc-technical-documentation-page":require("./fcc-technical-documentation-page.jpg"),"io-1":require("./io-1.jpg"),"io-2":require("./io-2.jpg"),"layout-1":require("./layout-1.jpg"),"layout-2":require("./layout-2.jpg"),"layout-3":require("./layout-3.jpg"),"layout-4":require("./layout-4.jpg"),"layout-6":require("./layout-6.jpg"),"layout-5":require("./layout-5.jpg"),"layout-7":require("./layout-7.jpg"),"layout-8":require("./layout-8.jpg"),"layout-9":require("./layout-9.jpg"),"react-emoji-generator":require("./react-emoji-generator.jpg"),"ui-features-section":require("./ui-features-section.jpg"),"ui-pricing-tables":require("./ui-pricing-tables.jpg"),"ui-product-card":require("./ui-product-card.jpg"),"ui-tabs":require("./ui-tabs.jpg"),"ui-user-profile-card":require("./ui-user-profile-card.jpg")};
+},{"./api-fetch-jobs.jpg":"tnYU","./fcc-celsius-to-fahrenheit-convertor.jpg":"YPXr","./fcc-product-landing-page.jpg":"MmsR","./fcc-survey-form.jpg":"eFgf","./fcc-tribute-page.jpg":"a340","./fcc-technical-documentation-page.jpg":"n1qK","./io-1.jpg":"iu3v","./io-2.jpg":"vE9J","./layout-1.jpg":"afdl","./layout-2.jpg":"AW2X","./layout-3.jpg":"GTvt","./layout-4.jpg":"eRGn","./layout-6.jpg":"suTb","./layout-5.jpg":"yfGi","./layout-7.jpg":"eO3I","./layout-8.jpg":"YMHG","./layout-9.jpg":"FkQ3","./react-emoji-generator.jpg":"obdG","./ui-features-section.jpg":"I2ti","./ui-pricing-tables.jpg":"LZNF","./ui-product-card.jpg":"vNbd","./ui-tabs.jpg":"FCE7","./ui-user-profile-card.jpg":"e4Wr"}],"QdeU":[function(require,module,exports) {
 "use strict";var e=s(require("../img/*.jpg"));function s(e){return e&&e.__esModule?e:{default:e}}(async()=>{if("loading"in HTMLImageElement.prototype){document.querySelectorAll("img[loading='lazy']").forEach(e=>{e.src=e.dataset.src})}else{const e=document.createElement("script");e.async=!0,e.src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.0/lazysizes.min.js",document.body.appendChild(e)}})();
 },{"../img/*.jpg":"m7Bz"}]},{},["QdeU"], null)
-//# sourceMappingURL=app.b6a000ee.js.map
+//# sourceMappingURL=app.4b7454a8.js.map
